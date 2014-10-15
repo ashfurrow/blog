469e696ad61fef6f64de234f941558ab18315dd3
Ash Furrow <ash@ashfurrow.com>
2014-10-15 22:56:27 +0200

    Installed Bootstrap.

--- config.rb
+++ config.rb
@@ -64,11 +64,11 @@ page "/feed.xml", layout: false
 #   end
 # end
 
-set :css_dir, 'stylesheets'
+set :css_dir, 'css'
 
 set :js_dir, 'javascripts'
 
-set :images_dir, 'images'
+set :images_dir, 'img'
 
 # Build-specific configuration
 configure :build do
