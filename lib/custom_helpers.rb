49edf157bdee4e890660183b7f62251d9e321d46
Ash Furrow <ash@ashfurrow.com>
2014-10-18 15:48:04 +0200

    Added header tag.

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -1,9 +1,12 @@
 module CustomHelpers
-  def html_title
-    if current_article
-    	(current_article.title + ' - ' + data.site.name) unless current_article.nil?
+  def html_title (current_article, current_resource)
+  	if current_resource
+  		title = current_resource.metadata[:page]["title"]
+  		return title + ' - ' + data.site.name unless title.nil?
+    elsif current_article
+    	return (current_article.title + ' - ' + data.site.name)
     end
 
-  	data.site.name
+	  data.site.name
   end
 end
