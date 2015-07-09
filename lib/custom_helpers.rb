cfa56bcaaee20300117f4b07444149241f5e8c28
Ash Furrow <ash@ashfurrow.com>
2015-07-09 14:13:53 -0400

    [Sitemap] Adds image links.

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -1,5 +1,5 @@
 module CustomHelpers
-  def html_title (current_article, current_resource)
+  def html_title(current_article, current_resource)
     if current_resource
       title = current_resource.metadata[:page]["title"]
       return title + ' - ' + data.site.name unless title.nil?
@@ -10,7 +10,7 @@ module CustomHelpers
     data.site.name
   end
 
-  def og_title (current_article, current_resource)
+  def og_title(current_article, current_resource)
     if current_resource
       title = current_resource.metadata[:page]["title"]
       return title unless title.nil?
@@ -20,24 +20,38 @@ module CustomHelpers
 
     data.site.name
   end
-    
-  def og_image (current_article, current_resource)
-    image = current_resource.metadata[:page]["og_image"]
-    image ||= current_resource.metadata[:page]["background_image"]
+
+  def og_image_or_default(current_article, current_resource)
+    image = og_image(current_resource)
 
     if current_article
       doc = Nokogiri::HTML(current_article.body)
       image ||= doc.xpath("//img").map { |img| img["src"] }.first
     end
 
+    image.makeAbsoluteURL! unless image.nil?
+
     # Default image
     image ||= data.site.dark_image
-
-    if !image.start_with? "http://"
-      image.prepend("http://ashfurrow.com")
-    end
 
     image
   end
 
+  def og_image(current_resource)
+    image = current_resource.metadata[:page]["og_image"]
+    image ||= current_resource.metadata[:page]["background_image"]
+
+    image.makeAbsoluteURL! unless image.nil?
+
+    image
+  end
+
+end
+
+class String
+  def makeAbsoluteURL!
+    if !start_with? "http://"
+      replace prepend("http://ashfurrow.com")
+    end
+  end
 end
--- source/sitemap.xml.builder
+++ source/sitemap.xml.builder
@@ -1,5 +1,5 @@
 xml.instruct!
-xml.urlset 'xmlns' => "http://www.sitemaps.org/schemas/sitemap/0.9" do
+xml.urlset 'xmlns' => "http://www.sitemaps.org/schemas/sitemap/0.9", 'xmlns:image' => "http://www.google.com/schemas/sitemap-image/1.1" do
   sitemap.resources.select { |page| 
     page.path =~ /\.html/ && !page.data.noindex == true 
   }.each do |page|
@@ -7,6 +7,15 @@ xml.urlset 'xmlns' => "http://www.sitemaps.org/schemas/sitemap/0.9" do
       xml.loc "http://ashfurrow.com#{page.url}"
       xml.lastmod Date.today.to_time.iso8601
       xml.changefreq page.data.changefreq || "monthly"
+
+      image_url = og_image(page)
+
+      if image_url
+        xml.tag! 'image:image' do |image|
+          image.tag! 'image:loc', image_url
+        end
+      end
+
       if page.url == '/'
         xml.priority "1.0"
       else
