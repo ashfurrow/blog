ec6d5622a647c6b61893a1e40a6c5da38a741f33
Ash Furrow <ash@ashfurrow.com>
2015-08-01 21:41:01 -0400

    [Custom Helpers] Adds make_narrow.

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -110,6 +110,24 @@ module CustomHelpers
 EOS
   end
 
+  def make_narrow(html)
+    <<-EOS
+</div>
+</div>
+
+<div class="row">
+<div class="col-lg-6 col-lg-offset-3 col-md-12">
+
+#{ html }
+
+</div>
+</div>
+
+<div class="row">
+<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+EOS
+  end
+
 end
 
 class String
