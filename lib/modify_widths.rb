ac1016bbedd380ce7b1974a27f9dbcd57c5d812b
Ash Furrow <ash@ashfurrow.com>
2015-08-11 19:34:37 -0400

    [Layouts] Switched posts to use standard layout.

--- lib/modify_widths.rb
+++ lib/modify_widths.rb
@@ -22,55 +22,56 @@ module ModifyWidths
 
       app.after_render do |body, path, locs, template_class|
 
-  def make_wide (contents)
-    <<-EOS
-  </div>
-</div>
+        wide_sizeclass = 'col-lg-10 col-lg-offset-1 col-md-12'
+        extra_wide_sizeclass = 'col-lg-12 col-md-12'
+        narrow_sizeclass = 'col-lg-6 col-lg-offset-3 col-md-12'
 
-<div class="row">
-  <div class="col-lg-10 col-lg-offset-1 col-md-12">
-    #{ contents }
-  </div>
-</div>
+        def make (sizeclass, contents)
+          <<-EOS
+            </div>
+          </div>
 
-<div class="row">
-  <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-EOS
-  end
+          <div class="row">
+            <div class="#{sizeclass}">
+              #{ contents }
+            </div>
+          </div>
 
-  def make_narrow (contents)
-    <<-EOS
-  </div>
-</div>
-
-<div class="row">
-  <div class="col-lg-6 col-lg-offset-3 col-md-12">
-    #{ contents }
-  </div>
-</div>
-
-<div class="row">
-  <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-EOS
-  end
+          <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+          EOS
+        end
         
         # There are multiple rendering calls and we want to get the one that renders the blog_post template. 
         if (path.to_s.index "blog_post") != nil
 
           wides = body.scan(/<p>BEGIN_WIDE<\/p>(.*?)<p>END_WIDE<\/p>/m).flatten
 
-          wides.each do |wide|
-            modified = make_wide(wide)
-            body.gsub!(wide, modified)
+          wides.each do |section|
+            body.gsub!(section, make(wide_sizeclass, section))
           end
 
           body.gsub!(/<p>(BEGIN|END)_WIDE<\/p>/, '')
 
+
+
+
+
+          extra_wides = body.scan(/<p>BEGIN_EXTRA_WIDE<\/p>(.*?)<p>END_EXTRA_WIDE<\/p>/m).flatten
+
+          extra_wides.each do |section|
+            body.gsub!(section, make(extra_wide_sizeclass, section))
+          end
+
+          body.gsub!(/<p>(BEGIN|END)_EXTRA_WIDE<\/p>/, '')
+
+
+
+
           narrows = body.scan(/<p>BEGIN_NARROW<\/p>(.*?)<p>END_NARROW<\/p>/m).flatten
 
-          narrows.each do |narrow|
-            modified = make_narrow(narrow)
-            body.gsub!(narrow, modified)
+          narrows.each do |section|
+            body.gsub!(section, modified = make(narrow_sizeclass, section))
           end
 
           body.gsub!(/<p>(BEGIN|END)_NARROW<\/p>/, '')
