0c1bde986603c9fa5a333e3e498944878c2854fa
Ash Furrow <ash@ashfurrow.com>
2015-08-29 12:58:05 -0400

    [Blog Theme] Removed unused styling.

--- source/javascripts/timeline.js
+++ source/javascripts/timeline.js
@@ -1,12 +1,12 @@
-$(document).ready(function(e){   
+$(document).ready(function(e){
   $('.cntl').cntl({
-    // The amount of "scroll padding" to allow 
+    // The amount of "scroll padding" to allow
     // The more, the later the state will be revealed
     revealbefore: 200,
     // The animate class
     // This class should have animation rules in css
     anim_class: 'cntl-animate',
     // A callback once the state has been revealed
-    onreveal: null 
+    onreveal: null
   });
 });
