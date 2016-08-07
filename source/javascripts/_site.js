f7a176e0e0df3eaeec4237ecd4bab71b3dffd226
Ash Furrow <ash@ashfurrow.com>
2016-08-07 14:17:54 -0400

    [Search] Adds documentation missing from #233.

--- source/javascripts/_site.js
+++ source/javascripts/_site.js
@@ -43,12 +43,14 @@ jQuery(document).ready(function($) {
       });
   }
 
-  // Download search json and set up.
+  // Download search index and then set up search.
+  // jQuery provides nicer syntax for this async download. 
   $.ajax({
     url: '/search.json',
     cache: true,
     method: 'GET',
     success: function(data) {
+      // Callback for success: log it and set up our search.
       console.log('Downloaded Search JSON.');
       setupSearch(data);
     }
@@ -59,31 +61,55 @@ var trackMaretingLink = function(link) {
    ga('send', 'event', 'marketing', link);
 }
 
-// Setup lunr index and callback for search-as-you-type. 
+// Setup lunrjs index and callback for search-as-you-type. 
 function setupSearch(lunrData) {
   console.log('Creating search index.');
+  // Set up the index, store it and the map in two vars defined up top.
+  // The index is used to make searches, the map is used to retrieve information
+  // about a result. This information is what's specified as "stored" in 
+  // config.rb's `activate :search` block.
   lunrIndex = lunr.Index.load(lunrData.index);
   lunrMap = lunrData.docs;
 
+  // We move the element with id `search` up in its DOM hierarchy and remove the
+  // search result group, a bootstrap element that shows the user the index is
+  // still being downloaded.
   $('#search').unwrap();
   $('#search-result-group').remove();
 
-  $("#search").bind("keyup", function(){
+  // We now bind the keyup event to an anonymous function, invoked whenever the
+  // user presses a key. We also trigger `keyup()` immediately, in case the user
+  // has typed a query before the index download has completed (see .keyup() at 
+  // the end of this function call).
+  $("#search").bind("keyup", function() {
+    // Every time, we clear the div with class `search-results`. 
     $(".search-results").empty();
 
     var query = $(this).val();
 
+    // We only want to search for queries that are more than two characters long.
     if (query.length <= 2) { return; }
 
-    var options = { year: "numeric", month: "long", day: "numeric" };
+    // Peform the search.
     var results = lunrIndex.search(query)
 
+    // Show 'no results' if the results are empty.
     if (results.length == 0) {
       $(".search-results").append('<p>No results.</p>');
     } else {
+      // For each result, execute this anyonymous function.
       $.each(results, function(index, result) {
+        // Retrieve the page that is represented by the result.
         page = lunrMap[result.ref];
-        date = new Date(page.date.match(/\d{4}-\d{2}-\d{2}/)).toLocaleDateString("en-US", options);
+        
+        // Extract the date using a regular expression and format according to
+        // the following date format options.
+        var dateOptions = { year: "numeric", month: "long", day: "numeric" };
+        date = new Date(page.date.match(/\d{4}-\d{2}-\d{2}/)).toLocaleDateString("en-US", dateOptions);
+
+        // Find the `search-results` div and append the following DOM elements.
+        // Ideally this would use Handlebars or some other client-side template
+        // library instead of raw string concatenation.
         $(".search-results").append(
           '<div class="result">' +
             '<a href="' + page.url + '">' + 
