70be75f613405cb4437fa8dd4c14ad75d4018c86
Ash Furrow <ash@ashfurrow.com>
2016-08-12 12:50:27 -0400

    [Middleman] Re-enabled redcarpet.

--- config.rb
+++ config.rb
@@ -9,8 +9,8 @@ require 'slim'
 # Blog settings
 ###
 
-# set :markdown_engine, :redcarpet
-# set :markdown, fenced_code_blocks: true, disable_indented_code_blocks: true, strikethrough: true, smartypants: true, with_toc_data: true
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true, disable_indented_code_blocks: true, strikethrough: true, smartypants: true, with_toc_data: true
 set :relative_links, true
 set :slim, :layout_engine => :slim
 
