7685dcbd35f0bab968d82d32e5f2be0c324dd747
Ash Furrow <ash@ashfurrow.com>
2019-03-23 20:52:54 -0400

    Basic UI customizations.

--- source/blog/2019-03-23-learning-from-other-programming-communities.html.markdown
+++ source/blog/2019-03-23-learning-from-other-programming-communities.html.markdown
@@ -69,12 +69,14 @@ While Apple might be able to provide a great foundation, _I'm a roads-and-bridge
 
 With all that said, with open hearts and clear minds, I'd like to share some of the practical affects of this rather philosophical distinction. What _are_ the actual advantages/disadvantages to both native iOS and JavaScript? In what's sure to make everyone mad, here are some concrete things I think make native iOS and JavaScript development both amazing and awful.
 
-<div class="iphone">
-  <div class="border">
-    <div class="responsive-html5-chat">
-    </div>
+BEGIN_NARROW
+
+<div class="chatparent">
+  <div class="responsive-html5-chat">
   </div>
 </div>
+
+END_NARROW
 
 This isn't a compressive list, and my opinions are constantly evolving. I try to be aware of my biases and blindspots, and I try not to assume that my experiences apply universally. These observations are obviously based on my experience of moving between iOS and web, which isn't typical.
 
--- source/javascripts/_responsiveChat.js
+++ source/javascripts/_responsiveChat.js
@@ -27,71 +27,66 @@ function responsiveChat(element) {
   showLatestMessage();
 
 
-  $(element + ' input[type="text"]').keypress(function (event) {
-      if (event.which == 13) {
-          event.preventDefault();
-          $(element + ' input[type="submit"]').click();
-      }
-  });
-  $(element + ' input[type="submit"]').click(function (event) {
-      event.preventDefault();
-      var message = $(element + ' input[type="text"]').val();
-      if ($(element + ' input[type="text"]').val()) {
-          var d = new Date();
-          var clock = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-          var month = d.getMonth() + 1;
-          var day = d.getDate();
-          var currentDate =
-              (("" + day).length < 2 ? "0" : "") +
-              day +
-              "." +
-              (("" + month).length < 2 ? "0" : "") +
-              month +
-              "." +
-              d.getFullYear() +
-              "&nbsp;&nbsp;" +
-              clock;
-          $(element + ' div.messages').append(
-              '<div class="message"><div class="myMessage"><p>' +
-              message +
-              "</p><date>" +
-              currentDate +
-              "</date></div></div>"
-          );
-          setTimeout(function () {
-              $(element + ' > span').addClass("spinner");
-          }, 100);
-          setTimeout(function () {
-              $(element + ' > span').removeClass("spinner");
-          }, 2000);
-      }
-      $(element + ' input[type="text"]').val("");
-      showLatestMessage();
-  });
+  // $(element + ' input[type="text"]').keypress(function (event) {
+  //     if (event.which == 13) {
+  //         event.preventDefault();
+  //         $(element + ' input[type="submit"]').click();
+  //     }
+  // });
+  // $(element + ' input[type="submit"]').click(function (event) {
+  //     event.preventDefault();
+  //     var message = $(element + ' input[type="text"]').val();
+  //     if ($(element + ' input[type="text"]').val()) {
+  //         var d = new Date();
+  //         var clock = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+  //         var month = d.getMonth() + 1;
+  //         var day = d.getDate();
+  //         var currentDate =
+  //             (("" + day).length < 2 ? "0" : "") +
+  //             day +
+  //             "." +
+  //             (("" + month).length < 2 ? "0" : "") +
+  //             month +
+  //             "." +
+  //             d.getFullYear() +
+  //             "&nbsp;&nbsp;" +
+  //             clock;
+  //         $(element + ' div.messages').append(
+  //             '<div class="message"><div class="myMessage"><p>' +
+  //             message +
+  //             "</p><date>" +
+  //             currentDate +
+  //             "</date></div></div>"
+  //         );
+  //         setTimeout(function () {
+  //             $(element + ' > span').addClass("spinner");
+  //         }, 100);
+  //         setTimeout(function () {
+  //             $(element + ' > span').removeClass("spinner");
+  //         }, 2000);
+  //     }
+  //     $(element + ' input[type="text"]').val("");
+  //     showLatestMessage();
+  // });
 }
 
-function responsiveChatPush(element, sender, origin, date, message) {
+function responsiveChatPush(element, sender, origin, message) {
   var originClass;
   if (origin == 'me') {
       originClass = 'myMessage';
   } else {
       originClass = 'fromThem';
   }
-  $(element + ' .messages').append('<div class="message"><div class="' + originClass + '"><p>' + message + '</p><date><b>' + sender + '</b> ' + date + '</date></div></div>');
+  $(element + ' .messages').append('<div class="message"><div class="' + originClass + '"><p>' + message + '</p></div></div>');
 }
 
 /* Activating chatbox on element */
 responsiveChat('.responsive-html5-chat');
 
-/* Let's push some dummy data */
-responsiveChatPush('.chat', 'Kate', 'me', '08.03.2017 14:30:7', 'It looks beautiful!');
-responsiveChatPush('.chat', 'John Doe', 'you', '08.03.2016 14:31:22', 'It looks like the iPhone message box.');
-responsiveChatPush('.chat', 'Kate', 'me', '08.03.2016 14:33:32', 'Yep, is this design responsive?');
-responsiveChatPush('.chat', 'Kate', 'me', '08.03.2016 14:36:4', 'By the way when I hover on my message it shows date.');
-responsiveChatPush('.chat', 'John Doe', 'you', '08.03.2016 14:37:12', 'Yes, this is completely responsive.');
 
-/* DEMO */
-if (parent == top) {
-$("a.article").show();
-}
+responsiveChatPush('.chat', 'Kate', 'me', 'It looks beautiful!');
+responsiveChatPush('.chat', 'John Doe', 'you', 'It looks like the iPhone message box.');
+responsiveChatPush('.chat', 'Kate', 'me', 'Yep, is this design responsive?');
+responsiveChatPush('.chat', 'Kate', 'me', 'By the way when I hover on my message it shows date.');
+responsiveChatPush('.chat', 'John Doe', 'you','Yes, this is completely responsive.');
 
