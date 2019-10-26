b012d8b6942799e12b5f48e370a19f1e89e22747
Ash Furrow <ash@ashfurrow.com>
2019-10-26 17:16:25 -0400

    Improvements for mobile.

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -8,9 +8,11 @@ export const Wrapper: any = styled.div`
   max-width: ${(props: any) => (props.fullWidth ? '100%' : '60rem')};
   padding: ${(props: any) => (props.fullWidth ? '0' : '0 6rem')};
   @media ${media.tablet} {
+    max-width: ${(props: any) => (props.fullWidth ? '100%' : '80rem')};
     padding: ${(props: any) => (props.fullWidth ? '0' : '0 3rem')};
   }
   @media ${media.phone} {
-    padding: ${(props: any) => (props.fullWidth ? '0' : '0 1rem')};
+    max-width: 100%;
+    padding: 0;
   }
 `
