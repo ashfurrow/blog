b0bf7043529b156c332c9c36ad45105c468dc6eb
Ash Furrow <ash@ashfurrow.com>
2019-10-27 10:14:20 -0400

    RSS feeds.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,20 @@
-require('source-map-support').install();
+require('source-map-support').install()
 require('ts-node').register({
   compilerOptions: {
     module: 'commonjs',
-    target: 'es2017',
-  },
-});
+    target: 'es2017'
+  }
+})
 
-const config = require('./config/SiteConfig').default;
-const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
+const config = require('./config/SiteConfig').default
+const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
 
 module.exports = {
   pathPrefix: config.pathPrefix,
   siteMetadata: {
-    siteUrl: config.siteUrl + pathPrefix,
+    title: config.siteTitle,
+    description: config.siteDescription,
+    siteUrl: config.siteUrl + pathPrefix
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -25,6 +27,96 @@ module.exports = {
     'gatsby-plugin-sitemap',
     'gatsby-plugin-lodash',
     {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          // Wish I knew a better way to duplicate the feed, haha.
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }]
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                  limit: 10,
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/feed.xml',
+            title: "Ash Furrow's Blog"
+          },
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }]
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                  limit: 10,
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/feed.rss.xml',
+            title: "Ash Furrow's Blog"
+          }
+        ]
+      }
+    },
+    {
       resolve: 'gatsby-plugin-mdx',
       options: {
         defaultLayouts: {
@@ -36,8 +128,8 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'post',
-        path: `${__dirname}/blog`,
-      },
+        path: `${__dirname}/blog`
+      }
     },
     {
       resolve: 'gatsby-plugin-web-font-loader',
@@ -53,8 +145,8 @@ module.exports = {
         id: config.Google_Tag_Manager_ID,
         // Include GTM in development.
         // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
-      },
+        includeInDevelopment: false
+      }
     },
     {
       resolve: 'gatsby-transformer-remark',
@@ -64,19 +156,19 @@ module.exports = {
             resolve: 'gatsby-remark-external-links',
             options: {
               target: '_blank',
-              rel: 'nofollow noopener noreferrer',
-            },
+              rel: 'nofollow noopener noreferrer'
+            }
           },
           'gatsby-remark-prismjs',
-          'gatsby-remark-autolink-headers',
-        ],
-      },
+          'gatsby-remark-autolink-headers'
+        ]
+      }
     },
     {
       resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: 'src/utils/typography.ts',
-      },
+        pathToConfigModule: 'src/utils/typography.ts'
+      }
     },
     {
       resolve: 'gatsby-plugin-manifest',
@@ -88,8 +180,8 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: 'standalone',
-        icon: config.favicon,
-      },
-    },
+        icon: config.favicon
+      }
+    }
   ]
-};
+}
