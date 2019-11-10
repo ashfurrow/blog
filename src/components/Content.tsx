025708412653795f4c30e47804cb22b9761940a8
Ash Furrow <ash@ashfurrow.com>
2019-11-09 22:36:20 -0500

    Migrates learning-from-communities post.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -21,8 +21,10 @@ export const Content = styled.div`
   }
   @media ${media.tablet} {
     padding: 0rem 3rem;
+    margin: 2rem 2rem;
   }
   @media ${media.phone} {
     padding: 1rem 0.5rem;
+    margin: 2rem 0;
   }
 `
