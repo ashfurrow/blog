b5e9cc5d65368e0b5389704efadc04ec3ed3ac89
Ash Furrow <ash@ashfurrow.com>
2022-03-19 21:46:03 -0300

    Removes Stylelint. Fixes missing peer dependencies.

--- src/components/ReactResponsiveEmbed.tsx
+++ src/components/ReactResponsiveEmbed.tsx
@@ -1,3 +1,5 @@
+// TODO: The source code for this is dead simple, grab it from the repo and just maintain it here.
+//       https://github.com/tableflip/react-responsive-embed/blob/master/src/index.js
 import _ReactResponsiveEmbed from 'react-responsive-embed'
 import React from 'react'
 import Wide from './Wide'
