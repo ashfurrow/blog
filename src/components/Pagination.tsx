a21cbb1a12f0798ad2d2a678208399865452ec31
Ash Furrow <ash@ashfurrow.com>
2022-08-01 12:57:12 -0300

    Fixes syntax errors.

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,85 +1,84 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
-import { media } from '../utils/media'
-import theme from '../config/Theme'
+import { media } from 'utils/media'
+import theme from 'config/Theme'
 import curriedDarken from 'polished/lib/color/darken'
 
 export const PaginationContainer = styled.div`
   text-align: center;
   margin: 2rem;
-  }
 `
 
 export const PaginationContent = styled.div`
-    display: inline-block;
-    padding: 0 2.5rem;
-    border-radius: 3.5rem;
-    background-color: #eee;
-    
-     @media ${media.phone} {
-      padding: 0 1rem;
-     }
+  display: inline-block;
+  padding: 0 2.5rem;
+  border-radius: 3.5rem;
+  background-color: #eee;
 
-    .page-numbers {
-      display: block;
-      float:left;
-      transition: 400ms ease;
-      color: ${theme.colors.grey.light};
-      letter-spacing: 0.1em;
-      padding: 1rem;
+  @media ${media.phone} {
+    padding: 0 1rem;
+  }
 
-      &:hover,
-      &.current {
-        background-color: ${curriedDarken(0.2, theme.colors.primary)};
-        color: ${theme.colors.white};
-      }
-      
-      &.prev {
-        margin-left: -1.5rem;
-      }
-      
-      &.next {
-        margin-right: -1.5rem;
-      }
+  .page-numbers {
+    display: block;
+    float: left;
+    transition: 400ms ease;
+    color: ${theme.colors.grey.light};
+    letter-spacing: 0.1em;
+    padding: 1rem;
 
-      &.prev:hover,
-      &.next:hover {
-        background-color: transparent;
-        color: ${curriedDarken(0.2, theme.colors.primary)};
-      }
-      
+    &:hover,
+    &.current {
+      background-color: ${curriedDarken(0.2, theme.colors.primary)};
+      color: ${theme.colors.white};
+    }
 
-      @media ${media.tablet} {
-        padding: 0 1.4rem;
-        display: none;
+    &.prev {
+      margin-left: -1.5rem;
+    }
 
-        &:nth-of-type(2) {
-          position: relative;
-          padding-right: 5rem;
+    &.next {
+      margin-right: -1.5rem;
+    }
 
-          &::after {
-            content: '...';
-            position: absolute;
-            top: 0;
-            left: 4.5rem;
-          }
-        }
+    &.prev:hover,
+    &.next:hover {
+      background-color: transparent;
+      color: ${curriedDarken(0.2, theme.colors.primary)};
+    }
 
-        &:nth-child(-n + 3),
-        &:nth-last-child(-n + 3) {
-          display: block;
-        }
+    @media ${media.tablet} {
+      padding: 0 1.4rem;
+      display: none;
 
-        &:nth-last-child(-n + 4) {
-          padding-right: 1.4rem;
+      &:nth-of-type(2) {
+        position: relative;
+        padding-right: 5rem;
 
-          &::after {
-            content: none;
-          }
+        &::after {
+          content: '...';
+          position: absolute;
+          top: 0;
+          left: 4.5rem;
         }
       }
-    `
+
+      &:nth-child(-n + 3),
+      &:nth-last-child(-n + 3) {
+        display: block;
+      }
+
+      &:nth-last-child(-n + 4) {
+        padding-right: 1.4rem;
+
+        &::after {
+          content: none;
+        }
+      }
+    }
+  }
+`
 
 interface Props {
   currentPage: number
