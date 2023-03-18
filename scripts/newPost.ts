6a5bf2561f24d429fe5f87908d52b27708eeca1b
Ash Furrow <ash.furrow@shopify.com>
2023-03-18 20:42:48 -0300

    Removes Tweet-backed background image lookup. [skip ci]

--- scripts/newPost.ts
+++ scripts/newPost.ts
@@ -1,7 +1,5 @@
-import Twitter, { AccessTokenOptions } from 'twitter'
-import { filter, first, last } from 'lodash'
-import http from 'http'
-import fs, { promises as fsPromises } from 'fs'
+import { last } from 'lodash'
+import fs from 'fs'
 import { generateSlug } from '../src/utils/paths'
 import moment from 'moment'
 
@@ -17,72 +15,18 @@ const main = async () => {
   const date = today.format('YYYY-MM-DD')
   const postPath = `${date}-${slug}`
 
-  const {
-    TWITTER_CONSUMER_KEY: consumer_key,
-    TWITTER_CONSUMER_SECRET: consumer_secret,
-    TWITTER_ACCESS_TOKEN: access_token_key,
-    TWITTER_ACCESS_SECRET: access_token_secret
-  } = process.env
-
-  const client = new Twitter({
-    consumer_key,
-    consumer_secret,
-    access_token_key,
-    access_token_secret
-  } as AccessTokenOptions)
-
-  const favourites = (await client.get('favorites/list', {
-    count: 100,
-    include_entities: true
-  })) as Tweet[]
-  const filtered = filter(favourites, (r) =>
-    ['CloudyConway', 'CrookedCosmos'].includes(r.user.screen_name)
-  )
-  // Grab the latest favourited tweet, or a first from CloudyConway
-  const tweet =
-    first(filtered) ||
-    first(
-      (await client.get('statuses/user_timeline', {
-        screen_name: 'CloudyConway',
-        include_entities: true,
-        count: 1
-      })) as Tweet[]
-    )
-
-  if (tweet) {
-    const media = first(tweet.entities.media)
-
-    await fsPromises.mkdir(`blog/${postPath}`, { recursive: true })
-    const file = fs.createWriteStream(`blog/${postPath}/background.png`)
-    if (media) {
-      http.get(media.media_url, (response) => response.pipe(file))
-    }
-    const bannnerAttribution = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
-    const post = `---
+  const post = `---
 title: ${title}
 date: ${date}
-banner: background.jpg
-bannerAttribution: ${bannnerAttribution}
 ---
 
 ${title}
 `
-    fsPromises.writeFile(`blog/${postPath}/index.mdx`, post, {
-      encoding: 'utf8'
-    })
-  }
-}
 
-interface Tweet {
-  user: {
-    screen_name: string
-  }
-  id_str: string
-  entities: {
-    media: {
-      media_url: string
-    }[]
-  }
+  await fs.promises.mkdir(`blog/${postPath}`, { recursive: true })
+  await fs.promises.writeFile(`blog/${postPath}/index.mdx`, post, {
+    encoding: 'utf8'
+  })
 }
 
 main()
