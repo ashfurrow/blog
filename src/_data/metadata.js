0985eae9bf651bccbc8118d425bc0c8baf41830b
Ash Furrow <ash@ashfurrow.com>
2025-06-29 23:42:34 -0300

    Metadata.

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,11 +1,10 @@
 export default {
-  title: "Eleventy Base Blog v9",
-  url: "https://example.com/",
+  title: "Ash Furrow",
+  url: "https://ashfurrow.com/",
   language: "en",
-  description: "I am writing about my experiences as a naval navel-gazer.",
+  description: "Compassionate Software Developer",
   author: {
-    name: "Your Name Here",
-    email: "youremailaddress@example.com",
-    url: "https://example.com/about-me/"
+    name: "Ash Furrow",
+    url: "https://ashfurrow.com/about/"
   }
 }
